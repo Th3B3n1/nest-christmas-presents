3b21b684fa36b4869cc1fcca291ed6dfeb89dd19
Th3B3n1 <146822610+Th3B3n1@users.noreply.github.com>
2024-12-06 15:22:16 +0100

    additional validation for toy dto

---
+++ prisma/seed.ts
@@ -0,0 +1,10 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function seed() {
+    let kids: kids = [];
+    for (let i = 0; i < 100; i++) {
+      kids.push();
+    }
+}
---
+++ src/prisma-client-exception/prisma-client-exception.filter.ts
@@ -0,0 +1,9 @@
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+
+@Catch()
+export class PrismaClientExceptionFilter<T> implements ExceptionFilter {
+  
+  catch(exception: T, host: ArgumentsHost) {
+    super.catch(exception, host);
+  }
+}
